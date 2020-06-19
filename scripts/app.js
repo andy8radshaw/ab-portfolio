878419baefc08283e748f4e67b9767b279aba3df
andy8radshaw <andy8radshaw@gmail.com>
2020-06-19 21:51:06 +0100

    added grid to portfolio

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,38 @@
-console.log('hello there')
+function init() {
+
+  // eslint-disable-next-line no-undef
+  new fullpage('#fullpage', {
+
+    
+    // navigation
+    menu: '#myMenu',
+    lockAnchors: false,
+    anchors: ['home', 'about', 'portfolio', 'contact'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['Home', 'About', 'Portfolio', 'Contact'],
+    showActiveTooltip: true,
+    slidesNavigation: true,
+
+    // scrolling
+    dragAndMove: true,
+    autoScrolling: true,
+    scrollingSpeed: 1000,
+    controlArrows: false,
+
+    // Accessibility
+    keyboardScrolling: true,
+    animateAnchor: false
+    // menu
+  })
+
+
+
+
+  //methods
+  // eslint-disable-next-line no-undef
+  fullpage_api.setAllowScrolling(true)
+
+}
+
+window.addEventListener('DOMContentLoaded', init)
